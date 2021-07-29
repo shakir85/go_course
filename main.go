cfe75d084b81a86d7e0028ee97ad16d07fc37c4d
shakir85 <sralmasaari@gmail.com>
2021-07-28 21:06:14 -0500

    End of section 1

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"myapp/doctor"
+	"os"
+	"strings"
+)
+
+func main() {
+	// Declare a reader, and specify where do you want to read from
+	reader := bufio.NewReader(os.Stdin)
+
+	whatToSay := doctor.Intro()
+
+	fmt.Println(whatToSay)
+
+	for {
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n')
+		// Replace the appended '\n' from user input (remove it)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(doctor.Response(userInput))
+		}
+
+	}
+
+}
