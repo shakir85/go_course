497acce439ab18b1f7f1b1b9115f9d8e9e6ecb01
shakir85 <sralmasaari@gmail.com>
2021-07-31 11:55:22 -0500

    Scope

---
+++ scope/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"myapp/packageone"
+)
+
+func main() {
+	var myString = "Test"
+	fmt.Println(myString)
+
+	newString := packageone.PublicVar
+	fmt.Println("From packageone", newString)
+
+}
---
+++ scope/packageone/package.go
@@ -0,0 +1,9 @@
+package packageone
+
+// private variables starts with lower-case letter
+// They Can not be accessed outside the package
+var privateVar = "I am private"
+
+// Public variables can be accessed outside the package
+// They start with an upper case letter
+var PublicVar = "I am public"
